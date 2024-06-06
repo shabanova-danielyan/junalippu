56247627a8496031d099182571f02a62bbcda751
Mykola Dotsenko <130664123+MykolaDotsenko@users.noreply.github.com>
2024-06-06 16:33:51 +0300

    log in window

--- src/components/PassengerInfo.tsx
+++ src/components/PassengerInfo.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
 const PassengerInfo: React.FC = () => {
-    return (
-        <div>
-            {/* form for log in */}
-            <form>
-                <h2>Login or Register</h2>
-                <label htmlFor="email">Email address:</label>
-                <input type="email" id="email" />
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" />
-                <button type="submit">Log in</button>
-                <button type="button">Create Account</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      {/* input */}
+      <form>
+        <h2>Log in</h2>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" />
+        <button type="submit">Log in</button>
+        <p>Do you have an account? If not, <a href="#">Create</a></p>
+        <button type="button">Cancel</button>
+        <button type="button">Back</button>
+      </form>
+    </div>
+  );
 }
 
 export default PassengerInfo;
